a2719e9065718c20feda649771992dbb0ff5b551
Joachim Hamraoui <joachim.hamraoui@student.ehb.be>
2020-05-11 13:12:04 +0200

    backup

--- app/app.js
+++ app/app.js
@@ -6,37 +6,60 @@ class AnimeList {
         this.searchValue = searchValue;
         this.formElement = document.getElementById('searchanime');
         this.formElementSubmit = document.getElementById('submitbtn');
-        console.log(this.formElement);
+        this.htmlElement = document.getElementById('animewrapper');
+        // console.log(this.formElement);
+        // console.log(this.htmlElement);
         // this.bindEvents();
     }
     async init() {
         await this.fetch();
         this.bindEvents();
-        // this.render();
     }
 
     async fetch() {
-        const response = await fetch(`${this.baseURL}/search/anime?q=${this.formElement.value}&page=1`);
-        const json = await response.json();
+        let response = await fetch(`${this.baseURL}/search/anime?q=${this.formElement.value}&page=1`);
+        let json = await response.json();
         this.searchResults = json.results;
+        console.log(json.results);
+        // console.log(this.searchResults);
     }
     getSearchedAnime(event) {
-        // console.log(this.formElement);
         event.preventDefault();
         console.log(`${this.baseURL}/search/anime?q=${this.formElement.value}&page=1`);
-        this.searchResults = this.searchResults;
+        // searchResults = this.searchResults;
         this.render();
     }
 
     render() {
         let htmlString = '';
         this.searchResults.forEach(anime => {
-            htmlString += anime.htmlString;
-        })
+            let searchInstance = new RenderSearch(anime);
+            htmlString += searchInstance.htmlString;
+        });
+        this.htmlElement.innerHTML = htmlString;
     }
 
     bindEvents() {
         this.formElementSubmit.addEventListener('click', this.getSearchedAnime.bind(this));
+    }
+}
+
+class RenderSearch {
+    constructor(data) {
+        this.data = data;
+    }
+
+    get htmlString() {
+        return `<div id="anime">
+        <div class="anime-img">
+            <img src="${this.data.image_url}" alt="${this.data.title}">
+        </div>
+        <h2>${this.data.title}</h2>
+        <h3>${this.data.start_date}</h3>
+        <p>${this.data.synopsis}
+        </p>
+        <a href="${this.data.url}">Meer over weten</a>
+    </div>`;
     }
 }
 
